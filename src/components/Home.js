1f6d987b2c75bfaa62381d2378cd43e81c9c66e2
Matt Wong <mattwong59@gmail.com>
2018-10-02 19:49:54 -0700

    Add HomePage boilerplate

---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const HomePage = () => 
+    <div>
+        <h1>HOME PAGE</h1>
+    </div>
+
+export default HomePage;
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LandingPage = () => 
     <div>
-        <h1>IT WORKS!</h1>
+        <h1>LANDING PAGE</h1>
     </div>
 
 export default LandingPage;
