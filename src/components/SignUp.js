d096f3bfe790295522eb1706450bac83ae75c30d
Matt Wong <mattwong59@gmail.com>
2018-11-12 17:53:00 -0800

    Add flex to p to move Sign Up Link down

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -106,7 +106,7 @@ class SignUpForm extends Component {
 }
 
 const SignUpLink = () =>
-    <p>
+    <p className="no-account">
         Don't have an account?
         {' '}
         <Link to={routes.SIGN_UP} className="secondary-btn">Sign Up</Link>
