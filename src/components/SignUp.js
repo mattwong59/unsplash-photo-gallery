523db860994768922f540beda7836ff80b3ae8f0
Matt Wong <mattwong59@gmail.com>
2018-11-13 19:17:26 -0800

    Add logic to SignUp to create a user in our realtime db

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link,
         withRouter,
          } from 'react-router-dom';
+
+import { auth, db } from '../firebase';        
 import { auth } from '../firebase';
 import * as routes from '../constants/routes';
 
@@ -44,8 +46,16 @@ class SignUpForm extends Component {
     
     auth.doCreateUserWithEmailAndPassword(email, passwordOne)
         .then(authUser => {
-            this.setState({...INITIAL_STATE});
-            history.push(routes.HOME);
+
+            //Create a user in your own accessible Firebase Database too
+            db.doCreateUser(authUser.user.uid, username, email)
+                .then(() => {
+                    this.setState({...INITIAL_STATE});
+                    history.push(routes.HOME);
+                })
+                .catch(error => {
+                    this.setState(byPropKey('error', error));
+                });
         })
         .catch(error => {
             this.setState(byPropKey('error', error));
